d751507cb732104f968e6d169011430b3ec35381
Yume <hei.hajatiana@gmail.com>
2024-01-13 15:02:58 +0300

    chore(dev): setup react-router-dom

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
+import {RouterProvider, createBrowserRouter} from "react-router-dom";
+import {Home} from "./pages/home";
 import "./index.css";
+
+const ROUTER = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={ROUTER} />
   </React.StrictMode>
 );
---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,7 @@
+import {FC} from "react";
+
+export const Home: FC = () => (
+  <div>
+    <h1 className="text-5xl">Hello world!</h1>
+  </div>
+);
