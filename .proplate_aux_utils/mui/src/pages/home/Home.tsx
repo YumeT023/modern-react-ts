44b6fd427656c9156633906914a783a33b029e25
Yume <hei.hajatiana@gmail.com>
2024-01-31 09:43:16 +0300

    feat: add ui framework opt and mui

---
+++ .proplate_aux_utils/mui/src/pages/home/Home.tsx
@@ -0,0 +1,8 @@
+import {FC} from "react";
+import {Box, Typography} from "@mui/material";
+
+export const Home: FC = () => (
+  <Box>
+    <Typography variant="h1">$project_name works</Typography>
+  </Box>
+);
